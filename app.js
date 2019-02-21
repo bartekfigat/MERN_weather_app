e019bb4fda8d3e955211d5de53de36b162f2b76d
Bartlomiej Figatowski <bartek_19_83@hotmail.com>
2019-02-20 23:02:08 -0500

    set back-end

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
+require("dotenv").config({ path: ".env" });
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//DB connect
+const db =
+  process.env.DB_PASSWOR || "mongodb://localhost:27017/mern_weather_app";
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => {
+    console.log("mongoDB connected");
+  })
+  .catch(err => {
+    console.error(
+      `message: ${err.message}  
+       codeN:${err.codeName} 
+       codeNumber:${err.code} 
+       errName:${err.name}
+      `
+    );
+  });
+
+app.use(morgan("tiny"));
+
 const port = process.env.PORT || 8080;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Server running: ${port}`));
