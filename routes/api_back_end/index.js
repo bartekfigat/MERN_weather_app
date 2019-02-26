59bb250e9872abfe779a635ab0366e0d048acf0b
Bartlomiej Figatowski <bartek_19_83@hotmail.com>
2019-02-25 20:14:41 -0500

    set limit per_page

--- routes/api_back_end/index.js
+++ routes/api_back_end/index.js
@@ -6,7 +6,7 @@ const fetch = require("node-fetch");
 
 const clientID = process.env.clientId;
 
-const urlUnsplash = `https://api.unsplash.com/search/photos/?page=1&limit=6&query=warsaw&client_id=${clientID}`;
+const urlUnsplash = `https://api.unsplash.com/search/photos/?page=1&per_page=6&query=warsaw&client_id=${clientID}`;
 
 router.get("/test", (req, res) =>
   //unsplash api
@@ -15,10 +15,9 @@ router.get("/test", (req, res) =>
       return res.json();
     })
     .then(data => {
-      //const a = data.map(image => {});
       const displayImages = [];
-      const limitDispaly = 4;
-      for (let i = limitDispaly; i < data.results.length; i++) {
+      // const limitDispaly = 4;
+      for (let i = 0; i < data.results.length; i++) {
         if (data.results.length[i] != "undefined") {
           console.log(data.results[i].urls.small);
           displayImages.push(data.results[i].urls.small);
